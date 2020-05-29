a6c22f4f70e9dcb1b9dbc713384c73c27f438b52
Kevin Bertrand <41152945+BertrandKevin@users.noreply.github.com>
2020-05-29 09:11:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import sys,os
 
 setup(
 	name = 'iotr-configure',
-	version = '0.0.1',
+	version = '0.0.2',
 	description = 'Configure the IOT router',
 	license='GPL v3',
 	author = 'Kevin Bertrand',
