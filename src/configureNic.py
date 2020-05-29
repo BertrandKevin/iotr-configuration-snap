9746467488411529d528a6900cd0950d6ba7fb74
Kevin Bertrand <41152945+BertrandKevin@users.noreply.github.com>
2020-05-29 14:28:25 +0200

    Update configureNic.py

--- src/configureNic.py
+++ src/configureNic.py
@@ -2,6 +2,7 @@
 
 import os
 import platform
+import subprocess
 
 import src.check_user_input as checkUserInput
 
@@ -12,14 +13,14 @@ def set_nic_settings(aftr, countryCode):
 		# Country Code configuration
 		try:
 			command = "sudo netmgr -i country_code set:" + countryCode
-			os.system(command)
+			subprocess.run(command)
 		except:
 			print("Le country code n'a pas pu etre configure!")
 
 		# AFTR configuration
 		try:
 			command = "sudo netmgr -i iotr aftr_address set " + aftr
-			os.system(command)
+			subprocess.run(command)
 		except:
 			print("L'AFTR n'a pas pu etre configuree!")
 
@@ -32,14 +33,14 @@ def set_nic_settings(aftr, countryCode):
 		# Country Code configuration
 		try:
 			command = "itron-edge.netmgr -i country_code set:" + countryCode
-			os.system(command)
+			subprocess.run(command)
 		except:
 			print("Le country code n'a pas pu etre configure!")
 
 		# AFTR configuration
 		try:
 			command = "itron-edge.netmgr -i aftr_address set " + aftr
-			os.system(command)
+			subprocess.run(command)
 		except:
 			print("L'AFTR n'a pas pu etre configuree!")
 
